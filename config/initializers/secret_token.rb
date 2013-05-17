800e55c5d2d27d551d7c9452d5a430ca5cac2a74
Carl Robinson <charlyblack75@gmail.com>
2013-05-17 07:12:38 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmsrefinerytest::Application.config.secret_token = 'c064bed3dbe42bc744c87eb9f6340f847dfcd255ce4aa3aedd7fec10d9625ed1a85a6fd709deaae04f0175d86afd05bf0a6248ae3b5b280d64549b9063f9a938'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cmsrefinerytest::Application.config.session_store :cookie_store, key: '_cmsrefinerytest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cmsrefinerytest::Application.config.session_store :active_record_store
